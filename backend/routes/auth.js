56e978497bd79db1865114b4a605eee250b0889b
Naman <dhurianmn01@gmail.com>
2026-07-28 23:40:21 +0530

    fixing login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -71,11 +71,11 @@ router.post("/login", async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+    httpOnly: true,
+    secure: false,        
+    sameSite: "lax",      
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     res.json({
       message: "Login successful",
